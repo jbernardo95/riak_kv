0dd32f7482f5210ec24b03a14599d28e754b15d9
Joao Bernardo <jb.amaro95@gmail.com>
2018-05-24 19:00:40 +0100

    Comments lagger logging and disk_log sync instructions

--- src/riak_kv_transactions_committer.erl
+++ src/riak_kv_transactions_committer.erl
@@ -89,7 +89,7 @@ root_commit(TransactionId, Puts, NValidations, Client, Conflicts, Lsn1) ->
         NValidations ->
             send_validation_result_to_client(TransactionId, Conflicts, Lsn, Client),
             send_validation_result_to_vnodes(TransactionId, Conflicts, Lsn, VnodePuts),
-            lager:info("Transaction ~p committed~n", [TransactionId]),
+            %lager:info("Transaction ~p committed~n", [TransactionId]),
             ets:delete(?RUNNING_TRANSACTIONS, TransactionId);
         _ ->
             ok
@@ -104,7 +104,7 @@ leaf_commit(TransactionId, Puts, 1 = _NValidations, Client, Conflicts, Lsn) ->
     Vnode = get_vnode(hd(Puts)),
     riak_kv_vnode:transaction_validation([Vnode], TransactionId, Puts, Conflicts, Lsn),
 
-    lager:info("Transaction ~p committed~n", [TransactionId]),
+    %lager:info("Transaction ~p committed~n", [TransactionId]),
 
     ets:delete(?RUNNING_TRANSACTIONS, TransactionId);
 
