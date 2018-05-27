263165401cc4a26f02d58d22850044c107ae6467
Joao Bernardo <jb.amaro95@gmail.com>
2018-05-27 10:29:55 +0100

    Eliminates the need for the tx manager to run inside the riak_kv cluster
    This commits creates the riak_core_gateway module which acts as a
    gateway to the riak_kv cluster metadata.
    
    With this module the transactions_manager can run outside of the cluster
    (in a separate machine for example) and still be able to access the
    cluster metadata to commit the transactions.

--- src/riak_kv_transactions_manager.erl
+++ src/riak_kv_transactions_manager.erl
@@ -2,7 +2,7 @@
 
 -behaviour(supervisor).
 
--export([start_link/1, validate_and_commit/7]).
+-export([start_link/1, connect_to_vnodes_cluster/1, validate_and_commit/7]).
 
 -export([init/1]).
 
@@ -12,6 +12,14 @@
 
 start_link(Id) ->
     supervisor:start_link({global, {?MODULE, Id}}, ?MODULE, Id).
+
+connect_to_vnodes_cluster(Id) ->
+    {ok, VnodeClusterGatewayNode} = application:get_env(riak_kv, vnode_cluster_gateway_node),
+
+    case net_adm:ping(VnodeClusterGatewayNode) of
+        pong -> riak_core_gateway:connect_to_vnodes_cluster(Id, VnodeClusterGatewayNode);
+        pang -> {error, vnode_cluster_gateway_node_unreachable}
+    end.
 
 validate_and_commit(HashedIdx, TransactionId, Snapshot, Gets, Puts, NValidations, Client) ->
     {ok, NLeafTransactionsManagers} = application:get_env(riak_kv, n_leaf_transactions_managers),
@@ -32,8 +40,11 @@ init(Id) ->
     Committer = {riak_kv_transactions_committer,
                  {riak_kv_transactions_committer, start_link, [Id]},
                  permanent, 5000, worker, [riak_kv_transactions_committer]},
+    RiakCoreGateway = {riak_core_gateway,
+                       {riak_core_gateway, start_link, [Id]},
+                       permanent, 5000, worker, [riak_core_gateway]},
 
     SupFlags = {one_for_one, 10, 10},
-    ChildSpecs = [Validator, Log, Committer],
+    ChildSpecs = [Validator, Log, Committer, RiakCoreGateway],
 
     {ok, {SupFlags, ChildSpecs}}.
