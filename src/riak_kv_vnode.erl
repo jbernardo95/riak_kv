b382d55427ce2c48749e8befb1b2758bda53a45b
Joao Bernardo <jb.amaro95@gmail.com>
2018-07-18 11:18:56 +0100

    Comments lagger logging instructions

--- src/riak_kv_vnode.erl
+++ src/riak_kv_vnode.erl
@@ -1353,11 +1353,11 @@ handle_exit(_Pid, Reason, State) ->
 handle_transactional_get_request(
   Req,
   Sender,
-  #state{idx = Idx, 
+  #state{idx = _Idx, 
          two_phase_commit_server = TwoPhaseCommitServer,
          pending_transactional_gets = PendingTransactionalGets} = State
 ) ->
-    lager:info("Handling transactional get request ~p at vnode ~p~n", [Req, Idx]),
+    %lager:info("Handling transactional get request ~p at vnode ~p~n", [Req, Idx]),
 
     Bkey = riak_kv_requests:get_bucket_key(Req),
     Snapshot = riak_kv_requests:get_snapshot(Req),
@@ -1378,7 +1378,7 @@ handle_transactional_get_request(
                                     % Selected content has not yet been committed
                                     % So save the request as pending until the transaction is committed
                                     TransactionId = riak_object:get_metadata_value(SnapshotConsistentContent, <<"transaction_id">>, -1),
-                                    lager:info("Selected version has not yet been committed, waiting for transaction ~p to be committed~n", [TransactionId]),
+                                    %lager:info("Selected version has not yet been committed, waiting for transaction ~p to be committed~n", [TransactionId]),
 
                                     case ets:lookup(PendingTransactionalGets, TransactionId) of
                                         [{TransactionId, PendingTransactionalGets}] ->
@@ -1478,10 +1478,10 @@ select_snapshot_consistent_content(Contents, Snapshot, true = _ReadOnly) ->
 handle_prepare_transaction_request(
   Req,
   Sender,
-  #state{idx = Idx,
+  #state{idx = _Idx,
          two_phase_commit_server = TwoPhaseCommitServer} = State
 ) ->
-    lager:info("Handling prepare transaction request ~p at vnode ~p from ~p~n", [Req, Idx, Sender]),
+    %lager:info("Handling prepare transaction request ~p at vnode ~p from ~p~n", [Req, Idx, Sender]),
 
     Snapshot = riak_kv_requests:get_snapshot(Req),
     Gets = riak_kv_requests:get_gets(Req),
@@ -1509,11 +1509,11 @@ handle_prepare_transaction_request(
 handle_commit_transaction_request(
   Req,
   Sender,
-  #state{idx = Idx,
+  #state{idx = _Idx,
          two_phase_commit_server = TwoPhaseCommitServer,
          pending_transactional_gets = PendingTransactionalGets} = State
 ) ->
-    lager:info("Handling commit transaction request ~p at vnode ~p from ~p~n", [Req, Idx, Sender]),
+    %lager:info("Handling commit transaction request ~p at vnode ~p from ~p~n", [Req, Idx, Sender]),
     
     Id = riak_kv_requests:get_id(Req),
     Gets = riak_kv_requests:get_gets(Req),
@@ -1549,10 +1549,10 @@ handle_commit_transaction_request(
 handle_prepare_commit_transaction_request(
   Req,
   Sender,
-  #state{idx = Idx,
+  #state{idx = _Idx,
          two_phase_commit_server = TwoPhaseCommitServer} = State
 ) ->
-    lager:info("Handling prepare commit transaction request ~p at vnode ~p from ~p~n", [Req, Idx, Sender]),
+    %lager:info("Handling prepare commit transaction request ~p at vnode ~p from ~p~n", [Req, Idx, Sender]),
 
     Snapshot = riak_kv_requests:get_snapshot(Req),
     Gets = riak_kv_requests:get_gets(Req),
